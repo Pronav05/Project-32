0f1a602e01d79f6d6f31e1b4ce3c46e0deec1dbe
Pronav05 <pforpranav05@gmail.com>
2020-11-19 17:29:08 +0800

    tower siege 3

--- sketch.js
+++ sketch.js
@@ -1,155 +1,162 @@
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
+var engine, world;
+var block1,stand,polygon,backgroundImg;
+var ground,polygon_img,slingshot,blackBg;
 
+var score = 0;
 
-var engine,world;
+function preload()
+{  
+  getBackgroundImg();
+  bg = loadImage("Orange.jpg");
+  polygon_img = loadImage("hexa 2.jpg");
+  blackBg = loadImage("Black Background.jpg");
+}
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(1200,400);
 
   engine = Engine.create();
   world = engine.world;
-
-  box1 = new BlueBox(500,360,20,30);
-  box2 = new YellowBox(520,360,20,30);
-  box3 = new GreenBox(540,360,20,30);
-  box4 = new RedBox(560,360,20,30);
-  box5 = new GreenBox(580,360,20,30);
-  box6 = new YellowBox(600,360,20,30);
-
-  box7 = new RedBox(510,330,20,30);
-  box8 = new YellowBox(530,330,20,30);
-  box9 = new BlueBox(550,330,20,30);
-  box10 = new GreenBox(570,330,20,30);
-  box11 = new RedBox(590,330,20,30);
-
-  box12 = new YellowBox(520,310,20,30);
-  box13 = new BlueBox(540,310,20,30);
-  box14 = new RedBox(560,310,20,30);
-  box15 = new GreenBox(580,310,20,30);
-
-  box16 = new YellowBox(530,280,20,30);
-  box17 = new RedBox(550,280,20,30);
-  box18 = new GreenBox(570,280,20,30);
-
-  box19 = new BlueBox(540,250,20,30);
-  box20 = new RedBox(560,250,20,30);
-
-  box21 = new GreenBox(550,220,20,30);
-
-  box22 = new GreenBox(500,135,20,30);
-  box23 = new BlueBox(520,135,20,30);
-  box24 = new YellowBox(540,135,20,30);
-  box25 = new RedBox(560,135,20,30);
-  box26 = new GreenBox(580,135,20,30);
-  box27 = new YellowBox(600,135,20,30);
-
-  box28 = new BlueBox(510,105,20,30);
-  box29 = new RedBox(530,105,20,30);
-  box30 = new YellowBox(550,105,20,30);
-  box31 = new GreenBox(570,105,20,30);
-  box32 = new RedBox(590,105,20,30);
-
-  box33 = new YellowBox(520,75,20,30);
-  box34 = new BlueBox(540,75,20,30);
-  box35 = new RedBox(560,75,20,30);
-  box36 = new GreenBox(580,75,20,30);
   
-  box37 = new YellowBox(530,45,20,30);
-  box38 = new BlueBox(550,45,20,30);
-  box39 = new GreenBox(570,45,20,30);
+  block1 = new Block(330,235,30,40);
+  block2 = new Block(360,235,30,40);
+  block3 = new Block(390,235,30,40);
+  block4 = new Block(420,235,30,40);
+  block5 = new Block(450,235,30,40);
+  block6 = new Block(360,195,30,40);
+  block7 = new Block(390,195,30,40);
+  block8 = new Block(420,195,30,40);
+  block9 = new Block(390,155,30,40);
+  block10 = new Block(930,235,30,40);
+  block11 = new Block(960,235,30,40);
+  block12 = new Block(990,235,30,40);
+  block13 = new Block(1020,235,30,40);
+  block14 = new Block(1050,235,30,40);
+  block15 = new Block(960,195,30,40);
+  block16 = new Block(990,195,30,40);
+  block17 = new Block(1020,195,30,40);
+  block18 = new Block(990,155,30,40);
 
-  ball = new Ball(0,0,50);
+  this.polygon = Bodies.circle(50,200,20);
+  World.add(world,this.polygon);
 
-  spring = new Projectile(ball.body,{x:200,y:200});
+  slingshot = new Slinglaunch(this.polygon,{x : 100,y: 200});
 
-  ground = new Platform(400,390,800,20);
-
-  groundHovering = new Platform(550,160,200,20);
+  stand = new Ground(1000,380,100000,20);
+  stand1 = new Ground(1000,250,200,15);
+  stand2 = new Ground(400,350,200,15);
   
 }
 
 function draw() {
-  background(247, 223, 178);
-  Engine.update(engine);  
+  if (backgroundImg)
+      background(backgroundImg);
 
- 
+      noStroke();
+
+  Engine.update(engine);
+
+  // Dragging and Releasing
+  textSize("20");
+  fill("Orange");
+  text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",200,50);
   
-   
 
-    box1.display();
-    box2.display();
-    box3.display();
-    box4.display();
-    box5.display();
-    box6.display();
+  // Second chance(Space key)
+  text("Press SPACE to get second chance",1000,350);
+  textSize("20");
+  fill("Orange");
 
-    box7.display();
-    box8.display();
-    box9.display();
-    box10.display();
-    box11.display();
+  //Score
+  text("SCORE :" +score,750,40);
+  textSize("20");
+  fill("Orange");
 
-    box12.display();
-    box13.display();
-    box14.display();
-    box15.display();
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display(); 
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  block13.display();
+  block14.display(); 
+  block15.display();
+  block16.display();
+  block17.display();
+  block18.display();
 
-    box16.display();
-    box17.display();
-    box18.display();
+  stand.display();
+  stand1.display();
+  stand2.display();
 
-    box19.display();
-    box20.display();
-
-    box21.display();
+  block1.score();
+  block2.score();
+  block3.score();
+  block4.score();
+  block5.score();
+  block6.score();
+  block7.score();
+  block8.score();
+  block9.score();
+  block10.score();
+  block11.score();
+  block12.score();
+  block13.score();
+  block14.score();
+  block15.score();
+  block16.score();
+  block17.score();
+  block18.score();
   
-    box22.display();
-    box23.display();
-    box24.display();
-    box25.display();
-    box26.display();
-    box27.display();
+  // Image
+  imageMode(CENTER);
+  image(polygon_img,this.polygon.position.x,this.polygon.position.y,40,40);
+  
+  slingshot.display();
+  
+}
 
-    box28.display();
-    box29.display();
-    box30.display();
-    box31.display();
-    box32.display();
-
-    box33.display();
-    box34.display();
-    box35.display();
-    box36.display();
-
-    box37.display();
-    box38.display();
-    box39.display();
-
-    ball.display();
-    spring.display();
-    ground.display();
-    groundHovering.display();
-
+function keyPressed()
+{
+  if (keyCode === 32)
+  {
+    slingshot.attach(this.polygon);
   }
+}
 
-  
-
-function mouseDragged(){
-  Matter.Body.setPosition(ball.body,{x:mouseX,y:mouseY});
+function mouseDragged() {
+  Matter.Body.setPosition(this.polygon,{x : mouseX, y : mouseY})
 }
 
 function mouseReleased(){
-  spring.release();
+slingshot.fly();
 }
 
-function keyPressed(){
-  if(keyCode === 32){
-    spring.attach(ball.body);
+async function getBackgroundImg(){
+  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJSON = await response.json();
+
+  var datetime = responseJSON.datetime;
+  var hour = datetime.slice(11,13);
+  
+  if(hour>=0600 && hour<=1900){
+      bg = "Orange.jpg";
+  }
+  else{
+      bg = "Black Background.jpg";
   }
 
- 
+  backgroundImg = loadImage(bg);
+  console.log(backgroundImg);
 }
